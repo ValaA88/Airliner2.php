d3ea821e481f7839fcc1bab3346f05f35f692c0e
ValaA88t <vala.arefi@gmail.com>
2024-06-18 10:31:34 +0200

    fileUpload

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function cleanInputs($var){
+  $data = trim($var);
+  $data = strip_tags($data);
+  $data = htmlspecialchars($data);
+
+  return($data);
+}
+
+function fileUpload($image){
+  if($image['error'] == 4){
+    $imageName = 'default.jpg';
+    $message = 'No image has been uploaded, please try again';
+  } else {
+    $checkIfImage = getimagesize($image["tmp_name"]);
+    $message = $checkIfImage ? "Ok" : "Not an Image";
+  }
+  if($message == "Ok"){
+    $ext = strtolower(pathinfo($image["name"],PATHINFO_EXTENSION));
+    $imageName =uniqid("").".".$ext;
+    move_uploaded_file($image["tmp_name"], "images/{$imageName}");
+
+  }
+
+  return[$imageName, $message];
+}
